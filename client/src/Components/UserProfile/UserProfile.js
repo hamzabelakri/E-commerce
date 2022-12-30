d4119ac420ad2122f849193fe58d5a58f8fb4a47
hamzabelakri <hamzabelakri92@gmail.com>
2022-12-30 12:56:13 +0100

    userprofile

--- client/src/Components/UserProfile/UserProfile.js
+++ client/src/Components/UserProfile/UserProfile.js
@@ -11,99 +11,184 @@ function UserProfile() {
       <h3>{user && user.email}</h3>
   </div> */
 
-    <main class="bg-gray-100 bg-opacity-25">
-      <div class="lg:w-8/12 lg:mx-auto mb-8">
-        <header class="flex flex-wrap items-center p-4 md:py-8">
-          <div class="md:w-3/12 md:ml-16">
-            <img
-              class="w-20 h-20 md:w-40 md:h-40 object-cover rounded-full
-                   border-2 border-pink-600 p-1"
-              src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-              alt="profile"
-            />
+    <div class="mt-10 sm:mt-0">
+      <div class="md:flex items-center justify-center">
+        <div class="mt-5 md:col-span-2 md:mt-0">
+          <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">
+              Complete Information
+            </h3>
+            <p class="mt-1 text-sm text-gray-600">
+              Use a permanent address where you can receive your order.
+            </p>
           </div>
-
-          <div class="w-8/12 md:w-7/12 ml-4">
-            <div class="md:flex md:flex-wrap md:items-center mb-4">
-              <h2 class="text-3xl inline-block font-light md:mr-2 mb-2 sm:mb-0">
-                {user && user.username}
-              </h2>
-
-              <a
-                href="#"
-                class="bg-blue-500 px-2 py-1 
-                      text-white font-semibold text-sm rounded block text-center 
-                      sm:inline-block "
-              >
-                Edit
-              </a>
-            </div>
-
-            <div class="hidden md:block">
-              <span>{user && user.email}</span>
-              <p>Lorem ipsum dolor sit amet consectetur</p>
-            </div>
-          </div>
-
-          <div class="md:hidden text-sm my-2">
-            <h1 class="font-semibold">Mr Travlerrr...</h1>
-            <span>Travel, Nature and Music</span>
-            <p>Lorem ipsum dolor sit amet consectetur</p>
-          </div>
-        </header>
-
-        <div class="px-px md:px-3">
-          <ul
-            class="flex items-center justify-around md:justify-center space-x-12  
-                  uppercase tracking-widest font-semibold text-xs text-gray-600
-                  border-t"
-          >
-            <li class="md:border-t md:border-gray-700 md:-mt-px md:text-gray-700">
-              <a class="inline-block p-3" href="#">
-                <i class="fas fa-th-large text-xl md:text-xs"></i>
-                <span class=" md:inline">postS</span>
-              </a>
-            </li>
-          </ul>
-
-          <div class="flex flex-wrap -mx-px md:-mx-3">
-            <div class="w-1/3 p-px md:px-3">
-              <a href="#">
-                <article class="post bg-gray-100 text-white relative pb-full md:mb-6">
-                  <img
-                    class="w-full h-full absolute left-0 top-0 object-cover"
-                    src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                    alt="image"
-                  />
-
-                  <i class="fas fa-square absolute right-0 top-0 m-1"></i>
-
-                  <div
-                    class="overlay bg-gray-800 bg-opacity-25 w-full h-full absolute 
-                              left-0 top-0 hidden"
-                  >
-                    <div
-                      class="flex justify-center items-center 
-                                  space-x-4 h-full"
+          <form action="#" method="POST">
+            <div class="">
+              <div class="bg-white px-4 py-5 sm:p-6">
+                <div class="grid grid-cols-6 gap-6">
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="first-name"
+                      class="block text-sm font-medium text-gray-700"
                     >
-                      <span class="p-2">
-                        <i class="fas fa-heart"></i>
-                        412K
-                      </span>
+                      First name
+                    </label>
+                    <input
+                      type="text"
+                      placeholder={user.username}
+                      name="first-name"
+                      id="first-name"
+                      autocomplete="given-name"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
 
-                      <span class="p-2">
-                        <i class="fas fa-comment"></i>
-                        2,909
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="last-name"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Last name
+                    </label>
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autocomplete="family-name"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-4">
+                    <label
+                      for="email-address"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Email address
+                    </label>
+                    <input
+                      type="text"
+                      placeholder={user.email}
+                      name="email-address"
+                      id="email-address"
+                      autocomplete="email"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="country"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Country
+                    </label>
+                    <select
+                      id="country"
+                      name="country"
+                      autocomplete="country-name"
+                      class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                      <option>United States</option>
+                      <option>Canada</option>
+                      <option>Mexico</option>
+                    </select>
+                  </div>
+
+                  <div class="col-span-6">
+                    <label
+                      for="street-address"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Street address
+                    </label>
+                    <input
+                      type="text"
+                      name="street-address"
+                      id="street-address"
+                      autocomplete="street-address"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                    <label
+                      for="city"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      City
+                    </label>
+                    <input
+                      type="text"
+                      name="city"
+                      id="city"
+                      autocomplete="address-level2"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label
+                      for="region"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      State / Province
+                    </label>
+                    <input
+                      type="text"
+                      name="region"
+                      id="region"
+                      autocomplete="address-level1"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label
+                      for="postal-code"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      ZIP / Postal code
+                    </label>
+                    <input
+                      type="text"
+                      name="postal-code"
+                      id="postal-code"
+                      autocomplete="postal-code"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                  <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label className="block text-sm font-medium text-gray-700">Photo</label>
+                    <div className="mt-1 flex items-center">
+                      <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
+                        <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg>
                       </span>
+                      <button
+                        type="button"
+                        className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        Change
+                      </button>
                     </div>
                   </div>
-                </article>
-              </a>
+                </div>
+              </div>
+              <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                <button
+                  type="submit"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Save
+                </button>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
 
