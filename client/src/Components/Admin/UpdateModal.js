b64b0545049ebab3a521ec3ecee9d7c17696048b
hamzabelakri <hamzabelakri92@gmail.com>
2023-01-08 18:25:04 +0100

    populated input

--- client/src/Components/Admin/UpdateModal.js
+++ client/src/Components/Admin/UpdateModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateUser } from "../../Redux/Actions/userAction";
 
@@ -10,6 +10,9 @@ function UpdateModal({ setShow }) {
     email: "",
     password: "",
   });
+  useEffect(() => {
+    if (state.user) setNewUser(state.user);
+  }, [state]);
   const handleChange = (event) => {
     setNewUser({ ...newUser, [event.target.name]: event.target.value });
   };
